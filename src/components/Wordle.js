84223c5ccb747af908fbfc23b502cbd0ce04e377
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-25 00:20:26 +0530

    feat: Wordle file created

---
+++ src/components/Wordle.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react'
+import useWordle from '../hooks/useWordle'
+
+export default function Wordle({ solution }) {
+
+    const { currentGuess, handleKeyUp} = useWordle(solution)
+
+    useEffect(() =>{
+        window.addEventListener('keyup',handleKeyUp);
+
+        return () =>window.removeEventListener('keyup',handleKeyUp);
+    })
+
+  return (
+    <div>Wordle</div>
+  )
+}
