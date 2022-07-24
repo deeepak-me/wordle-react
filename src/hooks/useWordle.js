c2d8d6c9e6636be8a8c058a38c546aba764235a7
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-25 00:43:58 +0530

    feat:handleKeyUp backspace completed

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -26,8 +26,21 @@ const useWordle = (solution) => {
     // handle keyup event & track current guess
     // if user presses enter, add the new guess
 
-    const handleKeyUp = () => {
+    const handleKeyUp = ({key}) => {
 
+        if(key === 'Backspace'){
+            setCurrentGuess((prev) =>{
+                return prev.slice(0,-1);
+            })
+            return
+        }
+        if(/^[A-Za-z]$/.test(key)){
+            if(currentGuess.length < 5){
+                setCurrentGuess((prev) => {
+                    return prev + key
+                })
+            }
+        }
     }
 
     return {turn, currentGuess,guesses,isCorrect, handleKeyUp}
