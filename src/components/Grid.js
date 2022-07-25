3b0a05ce8367bd47e83cba5e5b5123a1a1741c3c
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-26 00:17:49 +0530

    feat: adding grid file

---
+++ src/components/Grid.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Row from './Row'
+
+export default function Grid({guesses, currentGuess, turn}) {
+  return (
+    <div>
+        {guesses.map((g, i) =>{
+            return <Row key={{i}} />
+        })}
+    </div>
+  )
+}
