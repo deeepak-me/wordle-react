c2479beb5d1c4e1c6109ba55ff060cce4c62a165
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-08-10 16:11:36 +0530

    feat: arranging letter appearing format

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Row({guess}) {
+export default function Row({ guess, currentGuess }) {
 
-  if(guess){
-    return(
+  if (guess) {
+    return (
       <div className="row past">
         {guess.map((l, i) => (
           <div key={i} className={l.color}>{l.key}</div>
@@ -12,13 +12,29 @@ export default function Row({guess}) {
     )
   }
 
+  if (currentGuess) {
+    let letters = currentGuess.split('')
+
+    return (
+      <div className="row current">
+        {letters.map((letter, i) => (
+          <div key={i} className="filled">{letter}</div>
+        ))}
+        {[...Array(5 - letters.length)].map((_,i) => (
+          <div key={i}></div>
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <div className='row'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="row">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   )
+  
 }
