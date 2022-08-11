b7ec40c1e9aa28a0882f75bac8469f347ccadf50
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-08-12 00:50:50 +0530

    feat:Coloring Used Keys completed

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -7,6 +7,7 @@ const useWordle = (solution) => {
     const [guesses, setGuesses] = useState([...Array(6)]) //each guess is an array
     const [history, setHistory] = useState([]) //each guess is an string
     const [isCorrect, setIsCorrect] = useState(false)
+    const [usedKeys, setUsedKeys] = useState({}) //{a:'grey',b:'green',c:'yellow'}
 
 
     // format a guess into an array of letter objects 
@@ -55,6 +56,26 @@ const useWordle = (solution) => {
         setTurn((prevTurn) => {
             return prevTurn + 1
         })
+        setUsedKeys((prevUsedKeys) =>{
+            let newKeys = {...prevUsedKeys}
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if(l.color === 'green'){
+                    newKeys[l.key] = 'green'
+                    return
+                }
+                if(l.color === 'yellow' && currentColor !== 'green'){
+                    newKeys[l.key] = 'yellow'
+                    return
+                }
+                if(l.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow'){
+                    newKeys[l.key] = 'grey'
+                    return
+                }
+            })
+            return newKeys
+        })
         setCurrentGuess('')
     }
 
@@ -99,7 +120,7 @@ const useWordle = (solution) => {
         }
     }
 
-    return {turn, currentGuess,guesses,isCorrect, handleKeyUp}
+    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyUp}
 }
 
 export default useWordle
